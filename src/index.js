6c5acfefc4bcc85cc0ceba93c94935d276344220
Yuki <admin@yuki.sh>
2022-05-08 14:07:42 +0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,121 @@
+import { version } from '../package.json';
+import './style.scss';
+
+const classNames = {
+  chatPanel: 'chat-panel',
+  chatControls: 'controls',
+  chatMessage: 'chat-message',
+};
+const regex = {
+  chatPanelMarkup: /[\r\n]*(\s*)(<!-+\s+chat:\s*?start\s+-+>)[\r\n]+([\s|\S]*?)[\r\n\s]+(<!-+\s+chat:\s*?end\s+-+>)/m,
+  chatCommentMarkup: /[\r\n]*(\s*)#{1,6}\s*[*_]{2}\s*(.*[^\s])\s*[*_]{2}[\r\n]+([\s\S]*?)(?=#{1,6}\s*[*_]{2}|<!-+\s+chat:\s*?end\s+-+>)/
+};
+const setting = {
+  title: '聊天记录',
+  users: [],
+};
+
+function stringToColour(str) {
+  let hash = 0;
+  let colour = '#';
+
+  for (let i = 0; i < str.length; i++) {
+    hash = str.charCodeAt(i) + ((hash << 5) - hash);
+  }
+  for (let i = 0; i < 3; i++) {
+    const value = (hash >> (i * 8)) & 0xFF;
+    colour += ('00' + value.toString(16)).substr(-2);
+  }
+  return colour;
+}
+
+function generateAvatar(nickname) {
+  const colour = stringToColour(nickname);
+  const firstChar = nickname.substring(0, 1);
+  const userAvatar = `<div class="avatar" style="background-color: ${colour};">${firstChar}</div>`;
+
+  return userAvatar;
+}
+
+function renderChat(content, vm) {
+  let chatPanelMatch;
+  let chatMatch;
+
+  while (chatPanelMatch = regex.chatPanelMarkup.exec(content)) {
+    let chatPanel = chatPanelMatch[0];
+    let chatStartReplacement = '';
+    let chatEndReplacement = '';
+
+    const hasComments = regex.chatCommentMarkup.test(chatPanel);
+    const chatPanelStart = chatPanelMatch[2];
+    const chatPanelEnd = chatPanelMatch[4];
+    const chatControls = `
+      <div class="${classNames.chatControls}">
+        <div class="circle red"></div>
+        <div class="circle yellow"></div>
+        <div class="circle green"></div>
+        <div class="title">${setting.title}</div>
+      </div>
+    `;
+
+    if (hasComments) {
+      chatStartReplacement = `<div class="${classNames.chatPanel}">${chatControls}`;
+      chatEndReplacement = `</div>`;
+
+      while (chatMatch = regex.chatCommentMarkup.exec(chatPanel)) {
+        const nickname = chatMatch[2];
+        const message = chatMatch[3].trim();
+        const user = setting.users.filter(item => item.nickname === nickname)[0] ?? {};
+        const userAvatar = user.avatar
+          ? `<div class="avatar"><img src="${user.avatar}"></div>`
+          : generateAvatar(nickname);
+
+        chatPanel = chatPanel.replace(chatMatch[0], `
+          <div class="chat-content">
+            <div class="chat-message show">
+              ${userAvatar}
+              <div class="message-box">
+                <div class="nickname">${nickname}</div>
+                <div class="message">${message}</div>
+              </div>
+            </div>
+          </div>
+        `)
+      }
+    }
+    chatPanel = chatPanel.replace(chatPanelStart, chatStartReplacement);
+    chatPanel = chatPanel.replace(chatPanelEnd, chatEndReplacement);
+    content = content.replace(chatPanelMatch[0], chatPanel);
+  }
+  return content;
+}
+
+function docsifyChat(hook, vm) {
+  let hasChat = false;
+
+  hook.beforeEach(function (content) {
+    hasChat = regex.chatPanelMarkup.test(content);
+
+    if (hasChat) {
+      content = renderChat(content, vm);
+    }
+    return content;
+  });
+}
+
+if (window) {
+  window.$docsify = window.$docsify ?? {};
+  window.$docsify.chat = window.$docsify.chat ?? {};
+
+  Object.keys(window.$docsify.chat).forEach(key => {
+    if (Object.prototype.hasOwnProperty.call(setting, key)) {
+      setting[key] = window.$docsify.chat[key];
+    }
+  });
+
+  window.$docsify.chat.version = version;
+  window.$docsify.plugins = [].concat(
+    docsifyChat,
+    (window.$docsify.plugins ?? [])
+  );
+}
