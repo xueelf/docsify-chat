d021bb457c32576ed7127b94848893afbd55e487
xueelf <xueelf@outlook.com>
2023-11-10 11:51:41 +0800

    perf: lib size

--- build.js
+++ build.js
@@ -1,31 +1,83 @@
-import { build } from 'esbuild';
-import { sassPlugin } from 'esbuild-sass-plugin';
+import * as sass from 'sass';
+import * as esbuild from 'esbuild';
+import { readFile } from 'node:fs/promises';
 
 /**
- * @type {import('esbuild-sass-plugin').SassPluginOptions}
+ * 生成 style 模块
+ *
+ * @param {string} css - css 文本
+ * @returns 模块文本
  */
-const sassPluginOptions = {
-  type: 'style',
-};
+function styleModule(css) {
+  return `
+    const css = \`${css}\`;
+    const styleElement = document.createElement('style');
+    styleElement.textContent = css;
+
+    document.head.appendChild(styleElement);
+  `;
+}
+
+function svgPlugin() {
+  /**
+   * @type {esbuild.Plugin}
+   */
+  const svgPlugin = {
+    name: 'svg',
+    setup(build) {
+      build.onLoad({ filter: /\.svg$/ }, async args => {
+        const raw_svg = await readFile(args.path, 'utf-8');
+        const svg = raw_svg.replace(/(\n|\s{2})/g, '');
+
+        return {
+          contents: svg,
+          loader: 'text',
+        };
+      });
+    },
+  };
+  return svgPlugin;
+}
+
+function sassPlugin() {
+  /**
+   * @type {esbuild.Plugin}
+   */
+  const sassPlugin = {
+    name: 'sass',
+    setup(build) {
+      build.onLoad({ filter: /\.scss$/ }, async args => {
+        const { css } = await sass.compileAsync(args.path, {
+          style: 'compressed',
+        });
+
+        return {
+          contents: styleModule(css),
+        };
+      });
+    },
+  };
+  return sassPlugin;
+}
 
 /**
- * @type {import('esbuild').BuildOptions}
+ * @type {esbuild.BuildOptions}
  */
 const buildOptions = {
   bundle: true,
   entryPoints: ['src/index.ts'],
-  loader: {
-    '.svg': 'text',
-  },
-  plugins: [sassPlugin(sassPluginOptions)],
+  plugins: [sassPlugin(), svgPlugin()],
 };
 
-await build({
+await esbuild.build({
   ...buildOptions,
   outfile: 'lib/docsify-chat.js',
 });
-await build({
+
+await esbuild.build({
   ...buildOptions,
   minify: true,
   outfile: 'lib/docsify-chat.min.js',
 });
+
+console.log('✨🐢🚀✨');
